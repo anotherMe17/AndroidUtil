e98e66ec5a8933504404c21f08afddc78a6a34f9
lirenhao <anotherme@aliyun.com>
2017-02-21 22:11:53 +0800

    :trollface: 第一次提交

---
+++ android_util/src/main/java/io/github/anotherme17/android_util/Utils.java
@@ -0,0 +1,20 @@
+package io.github.anotherme17.android_util;
+
+import android.content.Context;
+
+import io.github.anotherme17.android_util.sp.SPUtils;
+
+/**
+ * @author AnotherMe17
+ */
+public class Utils {
+
+    private static Context context;
+    private static SPUtils spUtils;
+
+    private Utils() {
+        throw new UnsupportedOperationException("you can't instantiate me...");
+    }
+
+
+}
---
+++ android_util/src/main/java/io/github/anotherme17/android_util/sp/SPUtils.java
@@ -0,0 +1,11 @@
+package io.github.anotherme17.android_util.sp;
+
+/**
+ * @author AnotherMe17
+ */
+public class SPUtils {
+
+    private SPUtils() {
+        throw new UnsupportedOperationException("you can't instantiate me...");
+    }
+}
